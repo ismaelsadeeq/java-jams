b08c32d214588b1e9de04189bdbc74efc320019d
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2021-03-06 12:21:27 +0100

    setters and getters

--- classes/Time.java
+++ classes/Time.java
@@ -1,21 +1,36 @@
 public class Time {
   public int hour;
   public int minute;
-  public double second;
+  public double seconds;
 
   public Time(){
     this.hour = 0;
     this.minute = 0;
-    this.second = 0.0;
+    this.seconds = 0.0;
   }
 
   public Time(int hour,int minute, double seconds){
     this.hour = hour;
     this.minute = minute;
-    this.second = seconds;
+    this.seconds = seconds;
+  }
+  public int getHour(){
+    return this.hour;
+  }
+  public int getMinute(){
+    return this.hour;
+  }
+  public int getSeconds(){
+    return this.hour;
+  }
+
+  public void setHour(int hour){
+    this.hour = hour;
   }
   public static void main(String args[]){
     Time time = new Time(11, 50, 34.3);
-    System.out.println(time.minute);
+    System.out.println(time.hour);
+    Time time2 = new Time();
+    System.out.println("our time is "+time2.getHour()+time2.getMinute());
   }
 }
