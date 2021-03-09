35556560a232303caa3b8d31621c979772f2acf0
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2021-03-09 22:08:10 +0100

    guess

--- guess/Guess.java
+++ guess/Guess.java
@@ -4,7 +4,7 @@ public class Guess {
 
   public static boolean guess(int a){
     Random random = new Random();
-    int number = random.nextInt(100) + 1;
+    int number = random.nextInt(10) + 1;
     System.out.println("Your guess is : "+a);
     System.out.println("my guess is : "+number);
     if(number == a){
@@ -28,10 +28,10 @@ public class Guess {
     int win = 0;
     int lost = 0;
     while(status == true){
-      System.out.println("I ' m thinking of a number between 1 and 100");
+      System.out.println("I ' m thinking of a number between 1 and 10");
       System.out.println("Can you guess what it is?");
       Scanner num = new Scanner(System.in);
-      System.out.print("Type a number");
+      System.out.print("Type a number \n");
       int number = num.nextInt();
       num.nextLine();
       boolean game = guess(number);
